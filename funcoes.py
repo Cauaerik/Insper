6f0fc9139228f8c0616edac98971ddb9bcfd8765
Cauaerik <Cauaerik07@gmail.com>
2024-10-22 22:46:20 -0300

    preenchefrota2

--- funcoes.py
+++ funcoes.py
@@ -33,18 +33,8 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
   frotac = frota.copy()
   for nick, coord in frota.items():
-    if nome_navio not in nick:
-      frotac[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
     if nome_navio in nick:
       frotac[nome_navio] = coord + [define_posicoes(linha, coluna, orientacao, tamanho)]
   if frota == {}:
     frotac[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
   return frotac
-
-a = {'porta-aviões': [[[1, 2], [2, 2], [3, 2], [4, 2]], [[0, 4], [1, 4], [2, 4], [3, 4]]]}
-nome_navio = 'contratorpedeiro'
-linha = 5
-coluna = 4
-orientacao = 'vertical'
-tamanho = 2
-print(preenche_frota(a, nome_navio, linha, coluna, orientacao, tamanho))
