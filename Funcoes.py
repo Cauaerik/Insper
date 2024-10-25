eb4dbc4fe7efc02897c46dc54b41495e7ffaa22e
Kibbsr <73029421+Kibbsr@users.noreply.github.com>
2024-10-25 10:55:00 -0300

    Add files via upload

---
+++ Funcoes.py
@@ -0,0 +1,39 @@
+'''grid = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ]'''
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+  #posicoes sempre em ordem crescente
+  posicao = []
+  if orientacao == 'horizontal':
+    i = 0
+    while i < tamanho:
+      posicaox = linha 
+      posicaoy = coluna + i
+      posicao.append([posicaox, posicaoy])
+      i += 1
+  if orientacao == 'vertical':
+    i = 0
+    while i < tamanho:
+      posicaox = linha + i
+      posicaoy = coluna
+      posicao.append([posicaox, posicaoy])   
+      i += 1 
+  return posicao
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+      #adicionar o novo navio ao dicionario e atribuir o seu valor com a sua posicao 
+      if nome_navio in frota:
+            frota[nome_navio] += [define_posicoes(linha,coluna,orientacao,tamanho)]
+      else:
+            frota[nome_navio] = [define_posicoes(linha,coluna,orientacao,tamanho)]
+      return frota
